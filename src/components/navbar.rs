276e5b2b5cf6644310f891e5ada8f2c5c011494a
B3ngineering <98676125+B3ngineering@users.noreply.github.com>
2024-11-14 09:32:43 -0400

    Navbar finished, finally figured out how to get images working.

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -6,12 +6,18 @@ use crate::router::Route;
 pub fn navbar() -> Html {
     html! {
         <nav class="sidebar">
-            <img src="/static/images/blue_lock.png" alt="Logo" class="logo" />
+            <img src="/static/b.jpg" alt="Logo" class="logo" />
             <h1>{"Benjamin Young"}</h1>
             <div class="social-links">
-                <a href="https://youtube.com" target="_blank">{"YouTube"}</a>
-                <a href="https://linkedin.com" target="_blank">{"LinkedIn"}</a>
-                <a href="https://github.com" target="_blank">{"GitHub"}</a>
+                <a href="mailto:b9young@uwaterloo.ca" target="_blank">
+                    <img src="/static/mail.png" alt="Email" />
+                </a>
+                <a href="https://linkedin.com/in/ben-g-young/" target="_blank">
+                    <img src="/static/linkedin.png" alt="LinkedIn" />
+                </a>
+                <a href="https://github.com/B3ngineering" target="_blank">
+                    <img src="/static/github.png" alt="GitHub" />
+                </a>
             </div>
             <ul>
                 <li><Link<Route> to={Route::Home}>{"Home"}</Link<Route>></li>
